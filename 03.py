a1923eabc3c0dee02f18958c67850e48281342d6
pl <1285378614@qq.com>
2025-11-10 17:30:43 +0800

    03.py

--- 03.py
+++ 03.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 
 app = FastAPI()
 
-
+class Message(BaseModel):
+    message: str
+    name: str
+    
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
